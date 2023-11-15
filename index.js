5cf75d7d5170653fe1a4faf0293edba4ee2bacd1
Anupama_Halder <anupamahalder2022@gmail.com>
2023-11-15 15:36:58 +0530

    Create a get route for accessing service data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT||5000;
@@ -7,32 +9,38 @@ const port = process.env.PORT||5000;
 app.get('/',(req, res)=>{
     res.send('Nature Nurture server is running...');
 })
-
 app.listen(port,()=>{
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 })
-
 // DB URI 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vkjl2uu.mongodb.net/?retryWrites=true&w=majority`;
-// MongoDB connection 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sgpxxii.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 async function run() {
-    try {
-        //connect collection 
-        const natureDataCollection = client.db('natureNurtureDB').collection('servicesDB');
-        
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-    }
-  }
-run().catch(console.dir);
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // database connection
+    const servicesCollection = client.db('NatureDatabase').collection('servicesDB');
 
-// app.get('/api/v1/services',(req, res)=>{
-    // natureDataCollection.find()
-// })
+    app.get('/services', async(req, res)=>{
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
