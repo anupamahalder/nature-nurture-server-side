7d17263a33b7d62d86004601dbe7537aa2ddaa88
Anupama_Halder <anupamahalder2022@gmail.com>
2023-11-15 16:01:12 +0530

    Create bookings and user collection and post method to create data from client and send to database

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT||5000;
 
+// middleware 
+// parsers  to pasrsers data 
+app.use(express.json());
+
 app.get('/',(req, res)=>{
     res.send('Nature Nurture server is running...');
 })
@@ -26,15 +30,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // database connection
+    // create service connection
     const servicesCollection = client.db('NatureDatabase').collection('servicesDB');
+    // create booking collection 
+    const bookingsCollection = client.db('NatureDatabase').collection('bookingsDB');
+    // create user collection 
+    const useCollection = client.db('NatureDatabase').collection('usersDB');
 
-    app.get('/services', async(req, res)=>{
+    // get services data 
+    app.get('/api/v1/services', async(req, res)=>{
       const cursor = servicesCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
 
+    // create route for post bookings data 
+    app.post('/api/v1/user/create-booking', async(req, res)=>{
+      console.log(req.body);
+      const booking = req.body;
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    })
+    // create user data 
+    app.post('/api/v1/create-user', async(req, res)=>{
+      // console.log(req.body);
+      const userData = req.body;
+      const result = await useCollection.insertOne(userData);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
